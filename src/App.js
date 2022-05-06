63a55249f9788fa4d02fd5610e3ac61bff12139d
ParichayPai <parichaypai393@gmail.com>
2022-05-06 22:49:12 +0530

    navigation fix 1

--- src/App.js
+++ src/App.js
@@ -1,115 +1,186 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter,Switch, Route, Redirect} from 'react-router-dom';
-import  {Home}  from './pages/Home/Home';
-import { Navigation } from './Components/shared/Navigation/Navigation';
-import { Authenticate } from './pages/Authenticate/Authenticate';
-import { Activate } from './pages/Activate/Activate';
-import { Rooms } from './pages/Rooms/Rooms';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Navigation } from "./Components/shared/Navigation/Navigation";
+import { Authenticate } from "./pages/Authenticate/Authenticate";
+import { Activate } from "./pages/Activate/Activate";
+import { Rooms } from "./pages/Rooms/Rooms";
 
 const isAuth = false;
 const user = {
-  activated: false}
+  activated: false,
+};
 
 function App() {
   return (
     <BrowserRouter>
-    <Navigation/>
-    <Switch>
-            
-            <GuestRoute path="/authenticate" >
-              <Authenticate/>
-            </GuestRoute>
-            <SemiProtectedRoute path = "/activate">
-                <Activate/>
-            </SemiProtectedRoute>
-            <ProtectedRoute path = "/rooms">
-              <Rooms/>
-            </ProtectedRoute>
-            <GuestRoute path="/" exact>
-                <Home/>
-            </GuestRoute>
-        </Switch>
+      <Navigation />
+      <Routes>
+        <Route
+          path="/authenticate"
+          render={({ location }) => {
+            return isAuth ? (
+              <Navigate
+                to={{
+                  pathname: "/authenticate",
+                  state: { from: location },
+                }}
+              />
+            ) : null;
+          }}
+          element={<Authenticate />}
+        />
+        <Route
+          path="/rooms"
+          render={({ location }) => {
+            return !isAuth ? (
+              <Navigate
+                to={{
+                  pathname: "/",
+                  state: { from: location },
+                }}
+              ></Navigate>
+            ) : isAuth && !user.activated ? null : (
+              <Navigate
+                to={{
+                  pathname: "/rooms",
+                  state: { from: location },
+                }}
+              ></Navigate>
+            );
+          }}
+          element={<Activate />}
+        />
+        <Route
+          path="/"
+          exact
+          render={({ location }) => {
+            return isAuth ? (
+              <Navigate
+                to={{
+                  pathname: "/",
+                  state: { from: location },
+                }}
+              />
+            ) : null;
+          }}
+          element={<Home />}
+        />
+        <Route
+          path="/rooms"
+          render={({ location }) => {
+            return !isAuth ? (
+              <Navigate
+                to={{
+                  pathname: "/",
+                  state: { from: location },
+                }}
+              ></Navigate>
+            ) : isAuth && !user.activated ? (
+              <Navigate
+                to={{
+                  pathname: "/activate",
+                  state: { from: location },
+                }}
+              ></Navigate>
+            ) : null;
+          }}
+          element={<Rooms />}
+        />
+        {/* <GuestRoute path="/authenticate">
+          <Authenticate />
+        </GuestRoute> */}
+        {/* <SemiProtectedRoute path="/activate">
+          <Activate />
+        </SemiProtectedRoute> */}
+        {/* <ProtectedRoute path="/rooms">
+          <Rooms />
+        </ProtectedRoute> */}
+        {/* <GuestRoute path="/" exact>
+          <Home />
+        </GuestRoute> */}
+      </Routes>
     </BrowserRouter>
   );
 }
 
-const GuestRoute = ({children, ...rest}) => {
+const GuestRoute = ({ children, ...rest }) => {
   return (
-    <Route {...rest}
-      render={({location})=>{
-      return (isAuth ? 
-          (<Redirect to={
-              {
-                pathname: '/rooms',
-                state: {from: location}
-              }
-            }/>
-          ) :
-          (children)
-        )
-      }}>
-    </Route>
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return isAuth ? (
+          <Navigate
+            to={{
+              pathname: rest.path,
+              state: { from: location },
+            }}
+          />
+        ) : (
+          children
+        );
+      }}
+    ></Route>
   );
-}
-const SemiProtectedRoute = ({children, ...rest}) => {
- return(
-   <Route {...rest}
-   render = {({location})=>{
-    return (
-      !isAuth ? (
-        <Redirect to={{
-          pathname:'/',
-          state:{from : location}
-        }}>
-        </Redirect>
-      ):(
-        isAuth && !user.activated ? (
+};
+const SemiProtectedRoute = ({ children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return !isAuth ? (
+          <Navigate
+            to={{
+              pathname: "/",
+              state: { from: location },
+            }}
+          ></Navigate>
+        ) : isAuth && !user.activated ? (
           children
         ) : (
-          <Redirect
-          to={{
-            pathname:'/rooms',
-            state:{from : location}
-          }}></Redirect>
-        )
-      )
-    )
-   }}
-   
-   ></Route>
- )
-}
-
-const ProtectedRoute = ({children, ...rest}) => {
-  return(
-    <Route {...rest}
-    render = {({location})=>{
-     return (
-       !isAuth ? (
-         <Redirect to={{
-           pathname:'/',
-           state:{from : location}
-         }}>
- 
-         </Redirect>
- 
-       ):(
-         isAuth && !user.activated ? (
-          <Redirect
-          to={{
-            pathname:'/activate',
-            state:{from : location}
-          }}></Redirect>
-         ) : (
-           children
-         )
- 
-       )
-     )
-    }}
-    
+          <Navigate
+            to={{
+              pathname: "/rooms",
+              state: { from: location },
+            }}
+          ></Navigate>
+        );
+      }}
     ></Route>
-  )
- }
+  );
+};
+
+const ProtectedRoute = ({ children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return !isAuth ? (
+          <Navigate
+            to={{
+              pathname: "/",
+              state: { from: location },
+            }}
+          ></Navigate>
+        ) : isAuth && !user.activated ? (
+          <Navigate
+            to={{
+              pathname: "/activate",
+              state: { from: location },
+            }}
+          ></Navigate>
+        ) : (
+          children
+        );
+      }}
+    ></Route>
+  );
+};
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
-import {Link, useHistory} from 'react-router-dom'
-import styles from './Home.module.css'
-import { Card } from '../../Components/shared/Card/Card'
-import { Button } from '../../Components/shared/Button/Button'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
+import { Card } from "../../Components/shared/Card/Card";
+import { Button } from "../../Components/shared/Button/Button";
 export const Home = () => {
   const signInLinkStyle = {
-    color:'#0077ff',
-    fontWeight:'bold',
-    textDecoration:'none',
-    marginLeft:'10px'
-  }
-  const history = useHistory();
-  function startRegister(){
-    history.push('/authenticate');
+    color: "#0077ff",
+    fontWeight: "bold",
+    textDecoration: "none",
+    marginLeft: "10px",
+  };
+  const navigate = useNavigate();
+  function startRegister() {
+    navigate("/authenticate");
   }
   return (
-    
     <div className={styles.cardWrapper}>
       <Card title="Welcome to Codershouse!!!" icon="logo">
-
-      <p className={styles.text}>
-      We’re working hard to get Codershouse ready for everyone! 
-      While we wrap up the finishing youches, we’re adding people 
-      gradually to make sure nothing breaks :)
-    </p>
-    <div>
-      <Button onClick = {startRegister} text = "Let's Go"/>
-    </div>
-    <div className={styles.signinWrapper}>
-      <span className={styles.hasInvite}> Have an invite text? </span>
-    </div>
-
+        <p className={styles.text}>
+          We're working hard to get Codershouse ready for everyone! While we
+          wrap up the finishing youches, we're adding people gradually to make
+          sure nothing breaks :)
+        </p>
+        <div>
+          <Button onClick={startRegister} text="Let's Go" />
+        </div>
+        <div className={styles.signinWrapper}>
+          <span className={styles.hasInvite}> Have an invite text? </span>
+        </div>
       </Card>
-
     </div>
-    
-  )
-}
-
+  );
+};
